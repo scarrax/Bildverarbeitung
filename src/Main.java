e40f57ef848ef9605e51ab9ae6c9680cb2fae792
Niklas <niklas.huebner@huebner-verl.de>
2022-10-12 12:02:15 +0200

    opencv

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+
+import org.opencv.core.Core;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println(Core.VERSION);
+        System.out.println("load success");
     }
 }
