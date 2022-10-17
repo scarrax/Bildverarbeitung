e61051c1976f38ed60c660548d13c5357137238b
Niklas Hübner <niklas.huebner@huebner-verl.de>
2022-10-17 16:59:14 +0200

    add method convertMatToBufImg

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,37 @@
 
 import org.opencv.core.Core;
 import org.opencv.core.MatOfByte;
+
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.core.Mat;
+
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
+
 import java.io.InputStream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         initialiseOpenCv();
 
         // Reading the Image from the file and storing it in to a Matrix object
         String file = "Bilder/Erbsen.jpg";
         Mat image = Imgcodecs.imread(file);
 
-        // Encoding the image
-        MatOfByte matOfByte = new MatOfByte();
-        Imgcodecs.imencode(".jpg", image, matOfByte);
+        // Saves an Image
+        /*
+        String file2 = "Bilder/Erbsen2.jpg";
+        Imgcodecs.imwrite(file2, image);
+        System.out.println("Image Saved ............");
+        */
 
-        // Storing the encoded Mat in a byte array
-        byte[] byteArray = matOfByte.toArray();
+        BufferedImage image2 = convertMatToBufImg(image);
 
-        // Preparing the Buffered Image
-        InputStream in = new ByteArrayInputStream(byteArray);
-        BufferedImage bufImage = ImageIO.read(in);
-
-        displayJFrame(bufImage);
+        displayImage(image2);
 
         System.out.println("Image Loaded");
     }
@@ -43,7 +43,28 @@ public class Main {
         System.out.println("load success");
     }
 
-    public static void displayJFrame(BufferedImage bufImage){
+    public static BufferedImage convertMatToBufImg(Mat image) throws Exception{
+
+        // instantiating an empty MatOfByte class
+        MatOfByte matOfByte = new MatOfByte();
+
+        // Converting the Mat object to MatOfByte
+        Imgcodecs.imencode(".jpg", image, matOfByte);
+
+        // Converting MatOfByte into a byte array
+        byte[] byteArray = matOfByte.toArray();
+
+        // Preparing the InputStream object
+        InputStream in = new ByteArrayInputStream(byteArray);
+
+        // Preparing the BufferedImage
+        BufferedImage bufImage = ImageIO.read(in);
+
+        return bufImage;
+    }
+
+    public static void displayImage(BufferedImage bufImage){
+
         // Instantiate JFrame
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
